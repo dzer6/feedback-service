5f39cea7106223fa8e522b6ef4dfbf291c2342a6
Andrew Panfilov <andrew@dzer6.com>
2015-06-07 11:59:10 +0300

    add ring-cors

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [http-kit                       "2.1.19"]
                  [compojure                      "1.1.6"]
                  [ring                           "1.3.1"]
+                 [ring-cors                      "0.1.7"]
                  [org.clojure/tools.logging      "0.2.6"]
                  [org.slf4j/slf4j-api            "1.7.7"]
                  [org.slf4j/log4j-over-slf4j     "1.7.7"]
--- src/feedback/service/core.clj
+++ src/feedback/service/core.clj
@@ -3,22 +3,24 @@
   (:use feedback.service.middleware.json)
   (:require
     [org.httpkit.server :as http-kit-server]
+    [ring.middleware.cors :as rc]
     [compojure.core :refer (defroutes)]
-    [compojure.route :as route]
     [compojure.handler :as handler]
-    [ring.util.response :as rur]
     [clojure.tools.logging :as log]
     [feedback.service.controller.mail :as mail]))
 
 (def startup-params {:ip (or (System/getenv "OPENSHIFT_CLOJURE_HTTP_IP") "localhost")
                      :port (or (-> "OPENSHIFT_CLOJURE_HTTP_PORT" System/getenv read-string) 8080)})
 
+(def origin (-> "FEEDBACK_SERVICE_ORIGIN_HOST" System/getenv java.util.regex.Pattern/compile))
+
 (defroutes my-routes
   (JPOST  "/send" request mail/handle))
 
 (def my-ring-handler
   (-> my-routes
-      handler/site))
+      handler/site
+      (rc/wrap-cors :access-control-allow-origin [origin] :access-control-allow-methods [:get :put :post :delete])))
 
 (defn -main [& args]
   (log/info "startup params =" startup-params)
